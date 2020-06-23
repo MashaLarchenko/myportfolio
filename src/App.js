138f8a00a60042f8526ab1b88d00658a48d10d19
maria larchenko <mashalarchenko16@gmail.com>
2020-06-23 13:47:48 +0300

    feat: add history to route

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Portfolio from "./Components/Portfolio/Portfolio";
 import Contact from "./Components/Contact/Contact";
 import ProjectCard from './Components/Portfolio/ProjectCard';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import {createBrowserHistory} from 'history'
 import "./App.css";
 
 function App() {
+  const history = createBrowserHistory()
   return (
-    <Router>
+    <Router history={history}>
       <Route exact path="/" component={About} />
       <Route exact path="/portfolio" component={Portfolio} />
       <Route path="/contact" component={Contact} />
